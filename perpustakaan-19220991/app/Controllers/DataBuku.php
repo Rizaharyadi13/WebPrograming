4b0ba5f01c8cdb972ea556002e8ee5b57757c2aa
Rizaharyadi13 <rizaharyadi13@gmail.com>
2023-09-25 16:30:06 +0700

    Menghapus controller DataBuku

--- perpustakaan-19220991/app/Controllers/DataBuku.php
+++ perpustakaan-19220991/app/Controllers/DataBuku.php
@@ -4,10 +4,50 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 
-class DataBuku extends BaseController
+class PengelolaanDataBuku extends BaseController
 {
     public function index()
     {
-        //
+        // Kode untuk menampilkan daftar buku
+    }
+
+    public function katalogBuku()
+    {
+        // Kode untuk menampilkan katalog buku
+    }
+
+    public function informasiEksplisit()
+    {
+        // Kode untuk menampilkan informasi eksplisit buku
+    }
+
+    public function deskripsiBuku()
+    {
+        // Kode untuk menampilkan deskripsi buku
+    }
+
+    public function informasiPengarang()
+    {
+        // Kode untuk menampilkan informasi pengarang buku
+    }
+
+    public function kategoriKlasifikasi()
+    {
+        // Kode untuk menampilkan kategori dan klasifikasi buku
+    }
+
+    public function statusBuku()
+    {
+        // Kode untuk menampilkan status buku
+    }
+
+    public function dataPeminjaman()
+    {
+        // Kode untuk menampilkan data peminjaman buku
+    }
+
+    public function dataKondisi()
+    {
+        // Kode untuk menampilkan data kondisi buku
     }
 }
