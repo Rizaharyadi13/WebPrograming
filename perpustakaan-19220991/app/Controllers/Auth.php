7d418a4f85aaab88585981ab296ab522aa53acde
Irenesabilaputri <irenesabilaputri@gmail.com>
2023-09-25 13:01:53 +0700

    Tambahkan controller login

---
+++ perpustakaan-19220991/app/Controllers/Auth.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Auth extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+    
+    public function Login()
+    {
+        //
+    }
+
+    public function Daftar_Anggota_Baru()
+    {
+        //
+    }
+
+    public function User_Name()
+    {
+        //
+    }
+    
+    public function Password()
+    {
+        //
+    }
+
+    public function Lupa_Password()
+    {
+        //
+    }
+}
