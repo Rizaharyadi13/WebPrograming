3e626859e0ac52e702180e124432c503ba3feb0b
Rizaharyadi13 <rizaharyadi13@gmail.com>
2023-09-25 14:45:58 +0700

    Menambahkan controller PengelolaanDataBuku

---
+++ perpustakaan-19220991/app/Controllers/PengelolaanDataBuku.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class PengelolaanDataBuku extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
