18604e0f4b952213781e9489f9639bd0f8e50817
Rizaharyadi13 <rizaharyadi13@gmail.com>
2023-10-09 16:10:50 +0700

    Resolve konflik dalam README.md

--- app/Models/PeminjamanModel.php
+++ app/Models/PeminjamanModel.php
@@ -11,29 +11,28 @@ class ModelPinjaman extends Model
 
     // Tambahkan kolom yang diperbolehkan untuk digunakan dalam operasi CRUD
     protected $allowedFields = ['tgl_peminjaman', 'tgl_pengembalian', 'tb_pengguna_id_peminjaman', 'tb_pengguna_id_pengembalian', 'tb_anggota_id', 'tb_buku_id', 'denda'];
+protected $useTimestamps = false; // Jika Anda tidak menggunakan timestamp pada tabel
 
-    // Dates
-    protected $useTimestamps    = false;
-    protected $dateFormat       = 'datetime';
-    protected $createdField     = 'created_at';
-    protected $updatedField     = 'updated_at';
-    protected $deletedField     = 'deleted_at';
+    // Tambahkan relasi ke tabel lain jika diperlukan
+    protected $returnType = 'array';
 
-    // Validation
-    protected $validationRules  = [];
-    protected $validationMessages = [];
-    protected $skipValidation    = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks    = true;
-    protected $beforeInsert      = [];
-    protected $afterInsert       = [];
-    protected $beforeUpdate      = [];
-    protected $afterUpdate       = [];
-    protected $beforeFind        = [];
-    protected $afterFind         = [];
-    protected $beforeDelete      = [];
-    protected $afterDelete       = [];
+    // Contoh relasi dengan tabel tb_pengguna
+    protected $belongsTo = [
+        'pengguna_peminjaman' => [
+            'model' => 'PenggunaModel',
+            'foreign_key' => 'tb_pengguna_id_peminjaman',
+        ],
+        'pengguna_pengembalian' => [
+            'model' => 'PenggunaModel',
+            'foreign_key' => 'tb_pengguna_id_pengembalian',
+        ],
+        'anggota' => [
+            'model' => 'AnggotaModel',
+            'foreign_key' => 'tb_anggota_id',
+        ],
+        'buku' => [
+            'model' => 'BukuModel',
+            'foreign_key' => 'tb_buku_id',
+        ],
+    ];
 }
-
